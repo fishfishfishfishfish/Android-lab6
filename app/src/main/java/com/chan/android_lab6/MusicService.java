84739fc653a33debc9bdbf87cc2bf266c21dd4f2
fishfishfishfishfish <619156737@qq.com>
2017-11-26 23:48:51 +0800

    finished：使停止正常

--- app/src/main/java/com/chan/android_lab6/MusicService.java
+++ app/src/main/java/com/chan/android_lab6/MusicService.java
@@ -3,6 +3,7 @@ package com.chan.android_lab6;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
 import android.media.MediaPlayer;
 import android.os.Parcel;
@@ -11,9 +12,10 @@ import android.os.RemoteException;
 public class MusicService extends Service {
     public static MediaPlayer mp = new MediaPlayer();
     private MyBinder mBinder = new MyBinder();
+    private String musicPath = Environment.getExternalStorageDirectory()+"/data/melt.mp3";
     public MusicService() {
         try {
-            mp.setDataSource("/data/melt.mp3");
+            mp.setDataSource(musicPath);
             mp.prepare();
             mp.setLooping(true);
         }catch (Exception e){
@@ -43,9 +45,10 @@ public class MusicService extends Service {
                 case 102:
                     //stop
                     if(mp != null){
-                        mp.seekTo(0);
-                        mp.stop();  // 调用后MusicPlayer不能再播放音频
+//                        mp.stop();  // 调用后MusicPlayer不能再播放音频
+                        mp.reset();
                         try{
+                            mp.setDataSource(musicPath);
                             mp.prepare();
                             mp.seekTo(0);
                         }catch (Exception e){
@@ -64,11 +67,25 @@ public class MusicService extends Service {
                     //refresh UI, return time
                     reply.writeInt(mp.getCurrentPosition());
                     reply.writeInt(mp.getDuration());
+                    if(mp.isPlaying()){
+                        reply.writeInt(1);
+                    }else{
+                        reply.writeInt(0);
+                    }
                     break;
                 case 105:
                     //drag the process bar
                     int destination = data.readInt();
                     mp.seekTo(destination);
+                    break;
+                case 106:
+                    //got permission
+                    try {
+                        mp.setDataSource(musicPath);
+                        mp.prepare();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
                     break;
             }
             return super.onTransact(code, data, reply, flags);
